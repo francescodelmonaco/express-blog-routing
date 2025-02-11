e7b96ff3afd6beb228b5ba55ce2d90eba2cd2000
francescodelmonaco <francescodelmonaco1999@gmail.com>
2025-02-11 12:31:46 +0100

    import: posts.js into app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const postsRouter = require("./routers/posts");
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.use("/posts", postsRouter);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
--- routers/posts.js
+++ routers/posts.js
@@ -2,32 +2,32 @@ const express = require("express");
 const router = express.Router();
 
 // index
-router.get('/pizzas', function (req, res) {
+router.get('/', function (req, res) {
   res.send('Lista dei post');
 });
 
 // show
-router.get('/pizzas/:id', function (req, res) {
+router.get('/:id', function (req, res) {
   res.send('Dettagli del post ' + req.params.id);
 });
 
 // store
-router.post('/pizzas', function (req, res) {
+router.post('/', function (req, res) {
   res.send('Creazione nuovo post');
 });
 
 // update
-router.put('/pizzas/:id', function (req, res) {
+router.put('/:id', function (req, res) {
   res.send('Modifica integrale del post ' + req.params.id);
 });
 
 // modify
-router.patch('/pizzas/:id', function (req, res) {
+router.patch('/:id', function (req, res) {
   res.send('Modifica parziale del post ' + req.params.id);
 });
 
 // destroy
-router.delete('/pizzas/:id', function (req, res) {
+router.delete('/:id', function (req, res) {
   res.send('Eliminazione del post ' + req.params.id);
 });
 
